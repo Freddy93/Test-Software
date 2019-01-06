59aecc2779fd193f44f30db051be9fafa3c5dc34
Frederik Lentjes <frederik.lentjes@live.de>
2019-01-06 15:46:59 +0100

    Added Bogosort

---
+++ Test Software/Classes/BogoSort.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test_Software.Classes
+{
+    class BogoSort
+    {
+        private static Random rnd;
+
+        public static int Sort(int[] array)
+        {
+            int count = 0;
+
+            while (!isSorted(array))
+            {
+                Shuffle(array);
+                count++;
+            }
+
+            return count;
+        }
+
+        public static void Shuffle(int[] array)
+        {
+            if (rnd == null) rnd = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+                Swap(array, i, rnd.Next(0, array.Length - 1));
+        }
+
+        private static void Swap(int[] array, int item1, int item2)
+        {
+            int temp = array[item1];
+            array[item1] = array[item2];
+            array[item2] = temp;
+        }
+
+        private static bool isSorted(int[] array)
+        {
+            bool ret = true;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (((IComparable)array[i]).CompareTo(((IComparable)array[i + 1])) == 1)
+                {
+                    ret = false;
+                    break;
+                }
+            }
+
+            return ret;
+        }
+    }
+}
--- Test Software/Controllers/HomeController.cs
+++ Test Software/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Test_Software.Classes;
 
 namespace Test_Software.Controllers
 {
@@ -10,6 +11,22 @@ namespace Test_Software.Controllers
     {
         public ActionResult Index()
         {
+            int Min = 0;
+            int Max = 20;
+
+            // this declares an integer array with 5 elements
+            // and initializes all of them to their default value
+            // which is zero
+            int[] test2 = new int[5];
+
+            Random randNum = new Random();
+            for (int i = 0; i < test2.Length; i++)
+            {
+                test2[i] = randNum.Next(Min, Max);
+            }
+
+            BogoSort.Sort(test2);
+
             return View();
         }
 
