f7e8140651ba9f6902e341d7403d05cf00423992
Frederik Lentjes <frederik.lentjes@live.de>
2019-01-06 16:29:15 +0100

    FInalized Bogo

--- Test Software/Controllers/HomeController.cs
+++ Test Software/Controllers/HomeController.cs
@@ -11,21 +11,33 @@ namespace Test_Software.Controllers
     {
         public ActionResult Index()
         {
+            string resultsText = "Algorithmus:{0}<br>Zahlen zu Beginn:{1}<br>Dauer in Sekunden:{2}<br>Zahlen sortiert:{3}";
+
             int Min = 0;
-            int Max = 20;
+            int Max = 200;
 
             // this declares an integer array with 5 elements
             // and initializes all of them to their default value
             // which is zero
-            int[] test2 = new int[5];
+            int[] test2 = new int[11];
 
             Random randNum = new Random();
             for (int i = 0; i < test2.Length; i++)
             {
                 test2[i] = randNum.Next(Min, Max);
             }
+            string before = string.Join(",", test2);
+
+            DateTime start = DateTime.Now;
 
             BogoSort.Sort(test2);
+
+            DateTime end = DateTime.Now;
+
+            TimeSpan span = end - start;
+            double totalSeconds = span.TotalSeconds;
+
+            ViewBag.ResultText = string.Format(resultsText, "Bogosort", before, totalSeconds, string.Join(",", test2));
 
             return View();
         }
