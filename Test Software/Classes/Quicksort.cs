161a28aeba6b7bdae67e695ce1e0bd477c2e86f9
Frederik Lentjes <frederik.lentjes@live.de>
2019-01-06 16:08:39 +0100

    Added Quicksort

---
+++ Test Software/Classes/Quicksort.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test_Software.Classes
+{
+    public class Quciksort
+    {
+        public static void QuickSort(int[] a)
+        {
+            QuickSort(a, 0, a.Length - 1);
+        }
+
+
+        public static void QuickSort(int[] A, int left, int right)
+        {
+            if (left > right || left < 0 || right < 0) return;
+
+            int index = partition(A, left, right);
+
+            if (index != -1)
+            {
+                QuickSort(A, left, index - 1);
+                QuickSort(A, index + 1, right);
+            }
+        }
+
+        private static int partition(int[] A, int left, int right)
+        {
+            if (left > right) return -1;
+
+            int end = left;
+
+            int pivot = A[right];    // choose last one to pivot, easy to code
+            for (int i = left; i < right; i++)
+            {
+                if (A[i] < pivot)
+                {
+                    swap(A, i, end);
+                    end++;
+                }
+            }
+
+            swap(A, end, right);
+
+            return end;
+        }
+
+        private static void swap(int[] A, int left, int right)
+        {
+            int tmp = A[left];
+            A[left] = A[right];
+            A[right] = tmp;
+        }
+
+    }
+}
--- Test Software/Controllers/HomeController.cs
+++ Test Software/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Test_Software.Classes;
 
 namespace Test_Software.Controllers
 {
@@ -10,6 +11,34 @@ namespace Test_Software.Controllers
     {
         public ActionResult Index()
         {
+            string resultsText = "Algorithmus:{0}\nZahlen zu Beginn:{1}\nDauer in Sekunden:{2}\nZahlen sortiert:{3}";
+
+            int Min = 0;
+            int Max = 200;
+
+            // this declares an integer array with 5 elements
+            // and initializes all of them to their default value
+            // which is zero
+            int[] test2 = new int[100];
+
+            Random randNum = new Random();
+            for (int i = 0; i < test2.Length; i++)
+            {
+                test2[i] = randNum.Next(Min, Max);
+            }
+            string before = string.Join(",", test2);
+
+            DateTime start = new DateTime();
+
+            Quciksort.QuickSort(test2);
+
+            DateTime end = new DateTime();
+
+            TimeSpan span = end - start;
+            double totalSeconds = span.TotalSeconds;
+
+            ViewBag.ResultText = string.Format(resultsText, "Quicksort", before, totalSeconds, string.Join(",", test2));
+
             return View();
         }
 
